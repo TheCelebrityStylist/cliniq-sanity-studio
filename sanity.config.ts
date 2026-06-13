2abf85c09d2ee8c305fb831ba3d8c6d22c2bddaf
TheCelebrityStylist <elkejaspers.eac@gmail.com>
2026-06-13 11:42:42 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,14 +3,11 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || ''
-const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
-
 export default defineConfig({
   name: 'cliniq-maastricht',
   title: 'CLINIQ CMS',
-  projectId,
-  dataset,
+  projectId: 'jlnikrdc',
+  dataset: 'production',
   plugins: [structureTool(), visionTool()],
   schema: {
     types: schemaTypes,
